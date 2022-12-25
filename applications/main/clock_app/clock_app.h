96014f495f8c1a266621fe6e6aaa8acb16c44722
Ravex <thereal@ravextf.de>
2022-12-25 16:18:41 +0100

    Changed clock date format

--- applications/main/clock_app/clock_app.h
+++ applications/main/clock_app/clock_app.h
@@ -5,8 +5,8 @@
 
 #define TAG "Clock"
 
-#define CLOCK_ISO_DATE_FORMAT "%.4d-%.2d-%.2d"
-#define CLOCK_RFC_DATE_FORMAT "%.2d-%.2d-%.4d"
+#define CLOCK_ISO_DATE_FORMAT "%.4d.%.2d.%.2d"
+#define CLOCK_RFC_DATE_FORMAT "%.2d.%.2d.%.4d"
 #define CLOCK_TIME_FORMAT "%.2d:%.2d:%.2d"
 
 #define MERIDIAN_FORMAT "%s"
