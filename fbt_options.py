4305294f5a28597b8faec0ba349eb1bdb3f93520
Ravex <thereal@ravextf.de>
2023-01-15 22:08:15 +0100

    [FBT] Changed DIST_SUFFIX

--- fbt_options.py
+++ fbt_options.py
@@ -14,7 +14,7 @@ DEBUG = 0
 
 # Suffix to add to files when building distribution
 # If OS environment has DIST_SUFFIX set, it will be used instead
-DIST_SUFFIX = "CUFZ420FAP"
+DIST_SUFFIX = "HAUNTER"
 
 # Coprocessor firmware
 COPRO_OB_DATA = "scripts/ob.data"
